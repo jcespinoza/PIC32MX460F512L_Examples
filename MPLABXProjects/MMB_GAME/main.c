648ae01f576a25c208f35be9d2ee167e39982020
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-12-06 10:12:12 -0600

    Added Threads example

---
+++ MPLABXProjects/MMB_GAME/main.c
@@ -0,0 +1,169 @@
+#include <plib.h>
+#include "MMB.h"
+#include "LCDTerminal.h"
+#include "Graphics/Graphics.h"
+#include "HardwareProfile.h"
+
+// Configuration bits 
+#pragma config POSCMOD = XT, FNOSC = PRIPLL, FSOSCEN = ON
+#pragma config FPLLIDIV = DIV_2, FPLLMUL = MUL_20, FPLLODIV = DIV_1, FPBDIV = DIV_1
+#pragma config OSCIOFNC = ON, CP = OFF, BWP = OFF, PWP = OFF
+
+struct Sprite {
+    int x, y; //Position
+    int width, height; //Dimensions
+    BITMAP_FLASH *image;
+};
+
+// fonts
+extern const FONT_FLASH TerminalFont;
+
+/* Image bitmaps 4bpp */
+extern const unsigned char pacman_up[];
+extern const unsigned char pacman_down[];
+extern const unsigned char pacman_left[];
+extern const unsigned char pacman_right[];
+extern const unsigned char fruit1[];
+extern const unsigned char fruit2[];
+extern const unsigned char fruit3[];
+extern const unsigned char key[];
+
+BITMAP_FLASH pacman_up_bmp = {FLASH, pacman_up};
+BITMAP_FLASH pacman_down_bmp = {FLASH, pacman_down};
+BITMAP_FLASH pacman_left_bmp = {FLASH, pacman_left};
+BITMAP_FLASH pacman_right_bmp = {FLASH, pacman_right};
+
+BITMAP_FLASH fruit1_bmp = {FLASH, fruit1};
+BITMAP_FLASH fruit2_bmp = {FLASH, fruit2};
+BITMAP_FLASH fruit3_bmp = {FLASH, fruit3};
+BITMAP_FLASH key_bmp = {FLASH, key};
+
+/* End of bitmaps declarations */
+
+//Global variables
+volatile int count = 0;
+volatile int doRefresh = 0;
+struct Sprite pacman;
+struct Sprite food[4];
+
+//This routine gets executed every 40ms
+void __ISR(_TIMER_1_VECTOR, IPL3) Timer1_ISR(void)
+{
+    int key;
+
+    mT1ClearIntFlag();
+    count++;
+
+    key = MMBReadKey();
+
+    switch (key) {
+        case JOY_RIGHT: break;
+        case JOY_LEFT: break;
+        case JOY_UP: break;
+        case JOY_DOWN: break;
+        case JOY_SELECT: LD3 = !LD3; break;
+    }
+    
+    if (count == 25) { //1 second delay, blink LD0
+        LD0 = !LD0;
+        count = 0;
+    }
+}
+
+void StartTimer1()
+{
+    ConfigIntTimer1(T1_INT_ON | T1_INT_PRIOR_3 | T1_INT_SUB_PRIOR_1);
+    OpenTimer1(T1_ON | T1_IDLE_CON | T1_PS_1_256 | T1_SOURCE_INT, 12500);
+}
+
+void inline getBitmapDimensions(BITMAP_FLASH *bmp, int *w, int *h) {
+    const FLASH_WORD *bmpData = (FLASH_WORD *)bmp->address;
+
+    *w = bmpData[2];
+    *h = bmpData[1];
+}
+
+void InitSprites() {
+
+    //Packman
+    pacman.x = 0;
+    pacman.y = 0;
+    getBitmapDimensions(&pacman_right_bmp, &pacman.width, &pacman.height);
+    pacman.image = &pacman_right_bmp;
+
+    //Food
+    food[0].x = 60;
+    food[0].y = 60;
+    getBitmapDimensions(&fruit1_bmp, &food[0].width, &food[0].height);
+    food[0].image = &fruit1_bmp;
+
+    food[1].x = 260;
+    food[1].y = 60;
+    getBitmapDimensions(&fruit2_bmp, &food[1].width, &food[1].height);
+    food[1].image = &fruit2_bmp;
+
+    food[2].x = 260;
+    food[2].y = 180;
+    getBitmapDimensions(&fruit3_bmp, &food[2].width, &food[2].height);
+    food[2].image = &fruit3_bmp;
+
+    food[3].x = 60;
+    food[3].y = 180;
+    getBitmapDimensions(&key_bmp, &food[3].width, &food[3].height);
+    food[3].image = &key_bmp;
+}
+
+int main(void)
+{
+    int i, x, y;
+    
+    MMBInit(); // Initialize the MikroE MMB board
+    LCDInit();
+    InitSprites();
+    
+    PutImage(0, 0, &pacman_left_bmp, 1);
+    PutImage(pacman.width*1 + 2*1, 0, &pacman_right_bmp, 1);
+    PutImage(pacman.width*2 + 2*2, 0, &pacman_down_bmp, 1);
+    PutImage(pacman.width*3 + 2*3, 0, &pacman_up_bmp, 1);
+
+    x = 0;
+    y = pacman.height + 2;
+    for (i=0; i<4; i++) {
+        PutImage(x, y, food[i].image, 1);
+        x += food[i].width + 2;
+    }
+    
+    LCDSetXY(1, 4);
+    SetColor(BRIGHTRED);
+    LCDPutString("Welcome to PACMAN GAME");
+
+    LCDSetXY(1, 5);
+    LCDPutString("Width: ");
+    LCDWriteNumber(pacman.width);
+    
+    LCDSetXY(1, 6);
+    LCDPutString("Height: ");
+    LCDWriteNumber(pacman.height);
+
+    SetColor(WHITE);
+    LCDSetXY(1, 7);
+    LCDPutString("(move joystick to start)");
+
+    MMBGetKey();
+    
+    StartTimer1(); //Start Timer1
+
+    doRefresh = 1;
+    while (1) {
+        while (doRefresh == 0) {}
+
+        doRefresh = 0;
+
+        //TODO: Repaint screen
+    }
+
+}
+
+
+
+
---
+++ MPLABXProjects/MMB_GAME/nbproject/Makefile-default.mk
@@ -0,0 +1,223 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Include project Makefile
+ifeq "${IGNORE_LOCAL}" "TRUE"
+# do not include local makefile. User is passing all local related variables already
+else
+include Makefile
+# Include makefile containing local settings
+ifeq "$(wildcard nbproject/Makefile-local-default.mk)" "nbproject/Makefile-local-default.mk"
+include nbproject/Makefile-local-default.mk
+endif
+endif
+
+# Environment
+MKDIR=mkdir -p
+RM=rm -f 
+MV=mv 
+CP=cp 
+
+# Macros
+CND_CONF=default
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IMAGE_TYPE=debug
+OUTPUT_SUFFIX=elf
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/MMB_Game.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+else
+IMAGE_TYPE=production
+OUTPUT_SUFFIX=hex
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/MMB_Game.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+endif
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
+
+# Distribution Directory
+DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
+
+# Source Files Quoted if spaced
+SOURCEFILES_QUOTED_IF_SPACED=../../MMB/LCDTerminal.c ../../MMB/LCDTerminalFont.c ../../Microchip/Graphics/Primitive.c ../../Microchip/Graphics/DisplayDriver.c ../../MMB/MMB.c pacman_images.c main.c
+
+# Object Files Quoted if spaced
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1445236994/LCDTerminal.o ${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o ${OBJECTDIR}/_ext/375547254/Primitive.o ${OBJECTDIR}/_ext/375547254/DisplayDriver.o ${OBJECTDIR}/_ext/1445236994/MMB.o ${OBJECTDIR}/pacman_images.o ${OBJECTDIR}/main.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1445236994/LCDTerminal.o.d ${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o.d ${OBJECTDIR}/_ext/375547254/Primitive.o.d ${OBJECTDIR}/_ext/375547254/DisplayDriver.o.d ${OBJECTDIR}/_ext/1445236994/MMB.o.d ${OBJECTDIR}/pacman_images.o.d ${OBJECTDIR}/main.o.d
+
+# Object Files
+OBJECTFILES=${OBJECTDIR}/_ext/1445236994/LCDTerminal.o ${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o ${OBJECTDIR}/_ext/375547254/Primitive.o ${OBJECTDIR}/_ext/375547254/DisplayDriver.o ${OBJECTDIR}/_ext/1445236994/MMB.o ${OBJECTDIR}/pacman_images.o ${OBJECTDIR}/main.o
+
+# Source Files
+SOURCEFILES=../../MMB/LCDTerminal.c ../../MMB/LCDTerminalFont.c ../../Microchip/Graphics/Primitive.c ../../Microchip/Graphics/DisplayDriver.c ../../MMB/MMB.c pacman_images.c main.c
+
+
+CFLAGS=
+ASFLAGS=
+LDLIBSOPTIONS=
+
+############# Tool locations ##########################################
+# If you copy a project from one host to another, the path where the  #
+# compiler is installed may be different.                             #
+# If you open this project with MPLAB X in the new host, this         #
+# makefile will be regenerated and the paths will be corrected.       #
+#######################################################################
+# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
+FIXDEPS=fixDeps
+
+.build-conf:  ${BUILD_SUBPROJECTS}
+ifneq ($(INFORMATION_MESSAGE), )
+	@echo $(INFORMATION_MESSAGE)
+endif
+	${MAKE}  -f nbproject/Makefile-default.mk dist/${CND_CONF}/${IMAGE_TYPE}/MMB_Game.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+
+MP_PROCESSOR_OPTION=32MX460F512L
+MP_LINKER_FILE_OPTION=
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemble
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assembleWithPreprocess
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compile
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/_ext/1445236994/LCDTerminal.o: ../../MMB/LCDTerminal.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/_ext/1445236994 
+	@${RM} ${OBJECTDIR}/_ext/1445236994/LCDTerminal.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1445236994/LCDTerminal.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/1445236994/LCDTerminal.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -O3 -I"../../MMB" -I"../../Microchip/Include" -I"../../Microchip/Include/MDD File System" -I"../../Microchip/Include/Graphics" -I"." -MMD -MF "${OBJECTDIR}/_ext/1445236994/LCDTerminal.o.d" -o ${OBJECTDIR}/_ext/1445236994/LCDTerminal.o ../../MMB/LCDTerminal.c   
+	
+${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o: ../../MMB/LCDTerminalFont.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/_ext/1445236994 
+	@${RM} ${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -O3 -I"../../MMB" -I"../../Microchip/Include" -I"../../Microchip/Include/MDD File System" -I"../../Microchip/Include/Graphics" -I"." -MMD -MF "${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o.d" -o ${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o ../../MMB/LCDTerminalFont.c   
+	
+${OBJECTDIR}/_ext/375547254/Primitive.o: ../../Microchip/Graphics/Primitive.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/_ext/375547254 
+	@${RM} ${OBJECTDIR}/_ext/375547254/Primitive.o.d 
+	@${RM} ${OBJECTDIR}/_ext/375547254/Primitive.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/375547254/Primitive.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -O3 -I"../../MMB" -I"../../Microchip/Include" -I"../../Microchip/Include/MDD File System" -I"../../Microchip/Include/Graphics" -I"." -MMD -MF "${OBJECTDIR}/_ext/375547254/Primitive.o.d" -o ${OBJECTDIR}/_ext/375547254/Primitive.o ../../Microchip/Graphics/Primitive.c   
+	
+${OBJECTDIR}/_ext/375547254/DisplayDriver.o: ../../Microchip/Graphics/DisplayDriver.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/_ext/375547254 
+	@${RM} ${OBJECTDIR}/_ext/375547254/DisplayDriver.o.d 
+	@${RM} ${OBJECTDIR}/_ext/375547254/DisplayDriver.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/375547254/DisplayDriver.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -O3 -I"../../MMB" -I"../../Microchip/Include" -I"../../Microchip/Include/MDD File System" -I"../../Microchip/Include/Graphics" -I"." -MMD -MF "${OBJECTDIR}/_ext/375547254/DisplayDriver.o.d" -o ${OBJECTDIR}/_ext/375547254/DisplayDriver.o ../../Microchip/Graphics/DisplayDriver.c   
+	
+${OBJECTDIR}/_ext/1445236994/MMB.o: ../../MMB/MMB.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/_ext/1445236994 
+	@${RM} ${OBJECTDIR}/_ext/1445236994/MMB.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1445236994/MMB.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/1445236994/MMB.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -O3 -I"../../MMB" -I"../../Microchip/Include" -I"../../Microchip/Include/MDD File System" -I"../../Microchip/Include/Graphics" -I"." -MMD -MF "${OBJECTDIR}/_ext/1445236994/MMB.o.d" -o ${OBJECTDIR}/_ext/1445236994/MMB.o ../../MMB/MMB.c   
+	
+${OBJECTDIR}/pacman_images.o: pacman_images.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/pacman_images.o.d 
+	@${RM} ${OBJECTDIR}/pacman_images.o 
+	@${FIXDEPS} "${OBJECTDIR}/pacman_images.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -O3 -I"../../MMB" -I"../../Microchip/Include" -I"../../Microchip/Include/MDD File System" -I"../../Microchip/Include/Graphics" -I"." -MMD -MF "${OBJECTDIR}/pacman_images.o.d" -o ${OBJECTDIR}/pacman_images.o pacman_images.c   
+	
+${OBJECTDIR}/main.o: main.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/main.o.d 
+	@${RM} ${OBJECTDIR}/main.o 
+	@${FIXDEPS} "${OBJECTDIR}/main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -O3 -I"../../MMB" -I"../../Microchip/Include" -I"../../Microchip/Include/MDD File System" -I"../../Microchip/Include/Graphics" -I"." -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c   
+	
+else
+${OBJECTDIR}/_ext/1445236994/LCDTerminal.o: ../../MMB/LCDTerminal.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/_ext/1445236994 
+	@${RM} ${OBJECTDIR}/_ext/1445236994/LCDTerminal.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1445236994/LCDTerminal.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/1445236994/LCDTerminal.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -O3 -I"../../MMB" -I"../../Microchip/Include" -I"../../Microchip/Include/MDD File System" -I"../../Microchip/Include/Graphics" -I"." -MMD -MF "${OBJECTDIR}/_ext/1445236994/LCDTerminal.o.d" -o ${OBJECTDIR}/_ext/1445236994/LCDTerminal.o ../../MMB/LCDTerminal.c   
+	
+${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o: ../../MMB/LCDTerminalFont.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/_ext/1445236994 
+	@${RM} ${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -O3 -I"../../MMB" -I"../../Microchip/Include" -I"../../Microchip/Include/MDD File System" -I"../../Microchip/Include/Graphics" -I"." -MMD -MF "${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o.d" -o ${OBJECTDIR}/_ext/1445236994/LCDTerminalFont.o ../../MMB/LCDTerminalFont.c   
+	
+${OBJECTDIR}/_ext/375547254/Primitive.o: ../../Microchip/Graphics/Primitive.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/_ext/375547254 
+	@${RM} ${OBJECTDIR}/_ext/375547254/Primitive.o.d 
+	@${RM} ${OBJECTDIR}/_ext/375547254/Primitive.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/375547254/Primitive.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -O3 -I"../../MMB" -I"../../Microchip/Include" -I"../../Microchip/Include/MDD File System" -I"../../Microchip/Include/Graphics" -I"." -MMD -MF "${OBJECTDIR}/_ext/375547254/Primitive.o.d" -o ${OBJECTDIR}/_ext/375547254/Primitive.o ../../Microchip/Graphics/Primitive.c   
+	
+${OBJECTDIR}/_ext/375547254/DisplayDriver.o: ../../Microchip/Graphics/DisplayDriver.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/_ext/375547254 
+	@${RM} ${OBJECTDIR}/_ext/375547254/DisplayDriver.o.d 
+	@${RM} ${OBJECTDIR}/_ext/375547254/DisplayDriver.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/375547254/DisplayDriver.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -O3 -I"../../MMB" -I"../../Microchip/Include" -I"../../Microchip/Include/MDD File System" -I"../../Microchip/Include/Graphics" -I"." -MMD -MF "${OBJECTDIR}/_ext/375547254/DisplayDriver.o.d" -o ${OBJECTDIR}/_ext/375547254/DisplayDriver.o ../../Microchip/Graphics/DisplayDriver.c   
+	
+${OBJECTDIR}/_ext/1445236994/MMB.o: ../../MMB/MMB.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/_ext/1445236994 
+	@${RM} ${OBJECTDIR}/_ext/1445236994/MMB.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1445236994/MMB.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/1445236994/MMB.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -O3 -I"../../MMB" -I"../../Microchip/Include" -I"../../Microchip/Include/MDD File System" -I"../../Microchip/Include/Graphics" -I"." -MMD -MF "${OBJECTDIR}/_ext/1445236994/MMB.o.d" -o ${OBJECTDIR}/_ext/1445236994/MMB.o ../../MMB/MMB.c   
+	
+${OBJECTDIR}/pacman_images.o: pacman_images.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/pacman_images.o.d 
+	@${RM} ${OBJECTDIR}/pacman_images.o 
+	@${FIXDEPS} "${OBJECTDIR}/pacman_images.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -O3 -I"../../MMB" -I"../../Microchip/Include" -I"../../Microchip/Include/MDD File System" -I"../../Microchip/Include/Graphics" -I"." -MMD -MF "${OBJECTDIR}/pacman_images.o.d" -o ${OBJECTDIR}/pacman_images.o pacman_images.c   
+	
+${OBJECTDIR}/main.o: main.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/main.o.d 
+	@${RM} ${OBJECTDIR}/main.o 
+	@${FIXDEPS} "${OBJECTDIR}/main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -O3 -I"../../MMB" -I"../../Microchip/Include" -I"../../Microchip/Include/MDD File System" -I"../../Microchip/Include/Graphics" -I"." -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c   
+	
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compileCPP
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: link
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+dist/${CND_CONF}/${IMAGE_TYPE}/MMB_Game.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
+	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -mdebugger -D__MPLAB_DEBUGGER_PK3=1 -mprocessor=$(MP_PROCESSOR_OPTION)  -o dist/${CND_CONF}/${IMAGE_TYPE}/MMB_Game.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__ICD2RAM=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_PK3=1,-L"../../Test/C:/Program Files/Microchip/MPLAB C32/lib",-L"../../Test/C:/Program Files/Microchip/MPLAB C32/pic32mx/lib",-L".",-Map="${DISTDIR}/Test.X.${IMAGE_TYPE}.map"
+	
+else
+dist/${CND_CONF}/${IMAGE_TYPE}/MMB_Game.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
+	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -mprocessor=$(MP_PROCESSOR_OPTION)  -o dist/${CND_CONF}/${IMAGE_TYPE}/MMB_Game.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),-L"../../Test/C:/Program Files/Microchip/MPLAB C32/lib",-L"../../Test/C:/Program Files/Microchip/MPLAB C32/pic32mx/lib",-L".",-Map="${DISTDIR}/Test.X.${IMAGE_TYPE}.map"
+	${MP_CC_DIR}/xc32-bin2hex dist/${CND_CONF}/${IMAGE_TYPE}/MMB_Game.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} 
+endif
+
+
+# Subprojects
+.build-subprojects:
+
+
+# Subprojects
+.clean-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r build/default
+	${RM} -r dist/default
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+DEPFILES=$(shell "${PATH_TO_IDE_BIN}"mplabwildcard ${POSSIBLE_DEPFILES})
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
